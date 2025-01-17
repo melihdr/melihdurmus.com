e84126939f5dbd2ab056289b396af4d8529041f7
melihdr <meljhdurmus@gmail.com>
2025-01-17 15:54:07 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -53,4 +53,47 @@ function App() {
               <button
                 onClick={() => {
                   setContent(<Reading />);
-          
+                }}
+                className="button"
+              >
+                reading
+              </button>
+            </div>
+            <div>
+              <button
+                onClick={() => {
+                  setContent(<Sum23mer />);
+                }}
+                className="button"
+              >
+                2023 summer/ege
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div style={{ paddingBottom: "2vw" }}>
+          <div style={{ fontSize: "18px" }}>
+            <span style={{ fontWeight: "bold", fontSize: "18px" }}>
+              dreamscape
+            </span>
+            {""} by melih durmus
+          </div>
+          <hr />
+          <div className="top-buttons-div">
+            <button className="top-button">home</button>
+            <button className="top-button">about</button>
+            <button className="top-button">projects</button>
+            <button className="top-button">notes</button>
+            <button className="top-button">work</button>
+            <button className="top-button">reading</button>
+          </div>
+        </div>
+        <div className="right-grid">{content && <div>{content}</div>}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
